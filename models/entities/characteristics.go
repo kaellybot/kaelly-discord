2d41fc91d79efb017a72c6c158643be0e1584e0a
Steven Pescheteau <steven.pescheteau@gmail.com>
2024-10-07 12:13:04 +0200

    Characteristic emojis added for development mode

--- models/entities/characteristics.go
+++ models/entities/characteristics.go
@@ -2,7 +2,6 @@ package entities
 
 type Characteristic struct {
 	ID        string `gorm:"primaryKey"`
-	DebugName string
 	Emoji     string
 	SortOrder int `gorm:"unique"`
 }
--- repositories/characteristics/characteristics.go
+++ repositories/characteristics/characteristics.go
@@ -1,8 +1,12 @@
 package cities
 
 import (
+	"github.com/kaellybot/kaelly-discord/models/constants"
 	"github.com/kaellybot/kaelly-discord/models/entities"
 	"github.com/kaellybot/kaelly-discord/utils/databases"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
 )
 
 func New(db databases.MySQLConnection) *Impl {
@@ -10,7 +14,19 @@ func New(db databases.MySQLConnection) *Impl {
 }
 
 func (repo *Impl) GetCharacteristics() ([]entities.Characteristic, error) {
+	var response *gorm.DB
 	var characteristics []entities.Characteristic
-	response := repo.db.GetDB().Model(&entities.Characteristic{}).Find(&characteristics)
+	if viper.GetBool(constants.Production) {
+		response = repo.db.GetDB().
+			Model(entities.Characteristic{}).
+			Find(&characteristics)
+	} else {
+		log.Info().Msgf("Development mode enabled, retrieving characteristic debug emojis")
+		response = repo.db.GetDB().
+			Model(&entities.Characteristic{}).
+			Select("id, emoji_dev AS emoji, sort_order").
+			Find(&characteristics)
+	}
+
 	return characteristics, response.Error
 }
